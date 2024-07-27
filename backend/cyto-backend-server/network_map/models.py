a9cbfb93d8e1aaa630a038aaf83b9c8ad6e177c9
Jason-Lawrence <thejasonlawrence1@gmail.com>
2024-07-27 11:31:42 -0400

    Finish models

--- backend/cyto-backend-server/network_map/models.py
+++ backend/cyto-backend-server/network_map/models.py
@@ -5,13 +5,6 @@ from django.db import models
 from django.conf import settings
 
 
-class Layout(models.Model):
-    """
-    """
-    name = models.CharField(max_length=50)
-    data = models.JSONField(null=True, blank=True)
-
-
 class NetworkMap(models.Model):
     """
     Model for storing Network Maps.
@@ -20,14 +13,19 @@ class NetworkMap(models.Model):
         user (:obj: user.User): The User who created the Network Map.
         name (str): The name of the Network Map.
         description (str): The description of the Network Map.
+        layout (JSON): Store the Layout Properties. 
         is_public (bool): Flag for whether the Network Map is public to others.
             Defaults to False.
+        created_at (DateTime): When the Network Map was created.
+        last_updated (Datetime): When the Network Map was last updated.
     """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    layout = models.ForeignKey(Layout, on_delete=models.CASCADE)
+    layout = models.JSONField(default=dict)
     is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -38,8 +36,19 @@ class Node(models.Model):
     Model for storing nodes.
     
     Args:
-        name (str): The name of the Node.
-        parent (:obj: network_map.Node): The parent Node
+        network_map(:obj: network_map.models.NetworkMap): 
+            The Network Map the node belongs too.
+        id (str): The nodes Primary key.
+        parent (:obj: network_map.models.Node): The parent Node
+        x (float): The x position of the node.
+        y (float): The y position of the node.
+        selectable (bool): Whether the node's selection state is mutable.
+        locked (bool): Whether the node's position is locked.
+        grabbable (bool): Whether the node can be grabbed.
+        pannable (bool): Whether dragging the causes panning.
+        classes (str): Class names the node has.
+        style (JSON): Style property overrides.
+        scratch (JSON): Scratchpad data.
     """
     network_map = models.ForeignKey(
         NetworkMap,
@@ -69,6 +78,36 @@ class Node(models.Model):
 
 class Edge(models.Model):
     """
+    Model for storing Edges.
     
+    Args:
+        network_map(:obj: network_map.models.NetworkMap): 
+            The Network Map the node belongs too.
+        id (str): The Edges Primary Key.
+        source (:obj: network_map.models.Node):
+            The source node for the edge.
+        target(:obj: network_map.models.Node):
+            The target for the edge.
+        pannable (bool): Whether dragging on the edge can cause panning.
     """
+    network_map = models.ForeignKey(
+        NetworkMap,
+        related_name='edges',
+        on_delete=models.CASCADE
+    )
+    id = models.CharField(max_length=100, primary_key=True)
+    source = models.ForeignKey(
+        Node,
+        related_name='source_node',
+        on_delete=models.CASCADE
+    )
+    target = models.ForeignKey(
+        Node,
+        related_name='target_node',
+        on_delete=models.CASCADE
+    )
+    pannable = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.id
 
