2a7250ca1b09c3691913296020bf56d028ca139f
Jason-Lawrence <thejasonlawrence1@gmail.com>
2024-07-30 18:01:30 -0400

    Add example to cytoscape endpoint

--- backend/cyto-backend-server/network_map/views/network_map_views.py
+++ backend/cyto-backend-server/network_map/views/network_map_views.py
@@ -4,8 +4,10 @@ Views for the Network Map API.
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Q
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from drf_spectacular.utils import extend_schema, OpenApiExample
+from django.db.models import Q
+
 
 from .. import models, serializers
 
@@ -17,6 +19,50 @@ class NetworkMapViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
+    @extend_schema(
+        examples=[
+            OpenApiExample(
+                name='''Format the data for a network
+                    map into cytoscape JSON object.''',
+                description='Return Network Map in cytoscape format.',
+                summary='Cytoscape',
+                value={
+                    'layout': 'network_map.layout',
+                    'elements': [
+                        {
+                          'group': 'nodes',
+                          'data': {
+                              'id': 'node.nid',
+                              'label': 'node.label',
+                              'parent': 'node.parent'
+                          },
+                          'position':{
+                              'x': 'node.x',
+                              'y': 'node.y'
+                          },
+                          'selectable': 'node.selectable',
+                          'locked': 'node.locked',
+                          'grabbable': 'node.grabbable',
+                          'classes': 'node.classes',
+                          'style': 'node.style',
+                          'scratch': 'node.scratch'  
+                        },
+                        {
+                            'group': 'edges',
+                            'data':{
+                                'id': 'edge.eid',
+                                'label': 'edge.label',
+                                'source': 'edge.source.nid',
+                                'target': 'edge.target.nid'
+                            },
+                            'pannable': 'edge.pannable'
+                        }
+                    ]
+                },
+                response_only=True
+            )
+        ]
+    )    
     @action(detail=True)
     def cytoscape(self, request, pk=None):
         """Return the Network Map data in cytoscape format."""
