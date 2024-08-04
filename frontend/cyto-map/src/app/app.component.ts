6c8ba2e401e80ef315556d9cc9101c8ad67459e7
Jason-Lawrence <thejasonlawrence1@gmail.com>
2024-08-04 16:40:06 -0400

    add auto login

--- frontend/cyto-map/src/app/app.component.ts
+++ frontend/cyto-map/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService} from './auth/auth.service'
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cyto-map';
+  authService = inject(AuthService)
+
+  ngOnInit(): void {
+    this.authService.autoLogin()
+  }
 }
