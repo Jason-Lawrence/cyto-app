88cf2cf06bea2ab4237ded015bef5bfcaf2d5b1e
Jason-Lawrence <thejasonlawrence1@gmail.com>
2024-08-04 11:28:27 -0400

    finish register component

--- frontend/cyto-map/src/app/auth/register/register.component.ts
+++ frontend/cyto-map/src/app/auth/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = 'test@example.com';
+  password: string = '';
+  
+  constructor(private authService: AuthService, private router: Router) {}
 
+  onRegisterNewUser() {
+    let userData = {
+      'name': this.name,
+      'email': this.email,
+      'password': this.password
+    }
+    this.authService.registerNewUser(userData)
+  }
+
+  onSignIn(){
+    this.router.navigate(['login'])
+  }
 }
